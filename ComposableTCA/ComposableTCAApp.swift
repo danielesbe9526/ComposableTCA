ed3cbc0b18da6f7dec8be764da5de8917ed5463e
Daniel Beltran <danielesbe9526@hotmail.com>
2025-04-14 10:06:53 -0500

    Initial Commit

---
+++ ComposableTCA/ComposableTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableTCAApp.swift
+//  ComposableTCA
+//
+//  Created by Daniel Beltran on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ComposableTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
